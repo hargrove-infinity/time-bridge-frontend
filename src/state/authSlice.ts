92803fcdede188c0117ea1d75768f05b3b7b8ba7
hargrove <hargrove.infinity@gmail.com>
2025-11-08 18:39:22 +0200

    added token utils

--- src/state/authSlice.ts
+++ src/state/authSlice.ts
@@ -1,5 +1,6 @@
 import type { StateCreator } from "zustand";
 import { api, AUTH_ENDPOINTS } from "@/api";
+import { setToken } from "@/lib/token";
 
 interface AuthCredentials {
   email: string;
@@ -18,12 +19,16 @@ export const createAuthSlice: StateCreator<AuthSlice> = (set) => ({
   loadingLogin: false,
   register: async (body: AuthCredentials) => {
     set({ loadingRegister: true });
-    await api.post(AUTH_ENDPOINTS.REGISTER, body);
+    const res = await api.post(AUTH_ENDPOINTS.REGISTER, body);
+    const { payload } = res.data;
+    setToken(payload);
     set({ loadingRegister: false });
   },
   login: async (body: AuthCredentials) => {
     set({ loadingLogin: true });
-    await api.post(AUTH_ENDPOINTS.LOGIN, body);
+    const res = await api.post(AUTH_ENDPOINTS.LOGIN, body);
+    const { payload } = res.data;
+    setToken(payload);
     set({ loadingLogin: false });
   },
 });
